aedf9876b1138d2dc5172c0b921bbb82a3e0032b
Sean McEmerson <sean.mcemerson@pancentric.com>
2014-08-27 17:54:20 +0100

    Remove extraneous curl option

--- inc/functions.php
+++ inc/functions.php
@@ -1,13 +1,9 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 function cURL($url) {
     $ch = curl_init();
 
     curl_setopt_array($ch, array(
-        CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_URL => $url
     ));
